36fe9412777e9ebd719da5b41cd99e3f31d02248
Younghwan Mun <mcodegeeks@gmail.com>
2021-02-07 00:52:53 -0500

    add nginx

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -21,13 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r+9l!y&(l^av7*7z(k%@29k@ep_cpv+sazku(&u776ano&_b*2'
+SECRET_KEY = os.environ.get("SECRET_KEY", "will_never_guess")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=1))
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "mcodegeeks.com localhost 127.0.0.1 [::1]").split(" ")
 
 # Application definition
 
