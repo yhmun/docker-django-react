7aac02019a64e23c527c14efbba4bfcd864d2ee4
Younghwan Mun <mcodegeeks@gmail.com>
2021-02-10 20:40:32 -0500

    Added calling useEffect at the right time

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from  'react';
+import React, { useEffect, useState } from  'react';
 import ArticleList from '../components/ArticleList';
 import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
@@ -7,14 +7,24 @@ const ArticlePage = ({ match }) => {
   const name = match.params.name;
   const article = articleContent.find(article => article.name == name);
   
+  const [articleInfo, setArticleInfo] = useState({
+    upvotes: 0,
+    comments: []
+  });
+
+  useEffect(() => {
+    setArticleInfo({ upvotes: Math.ceil(Math.random() * 10) })
+  }, [name]);
+
   if (!article)
     return <NotFoundPage />
 
   const otherArticles = articleContent.filter(article => article.name !== name);
-  
+
   return (
     <React.Fragment>
       <h2>{article.title}</h2>
+      <p>This post has been upvoted {articleInfo.upvotes} times</p>
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
