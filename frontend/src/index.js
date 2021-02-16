c7543cacd1711949a605f374ddd9ad4c3bae0343
Younghwan Mun <mcodegeeks@gmail.com>
2021-02-16 15:37:09 -0500

    Add jquery and enable bootstrap script

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/js/bootstrap.js';
 
 ReactDOM.render(
   <React.StrictMode>
