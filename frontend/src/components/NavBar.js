9ecd89fd0b37aca802d3cc2aa85184d8a49789d6
Younghwan Mun <mcodegeeks@gmail.com>
2021-02-18 17:35:12 -0500

    Applying material-ui

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,8 +1,173 @@
-import React, { Component } from 'react';
+import clsx from 'clsx';
+import { Component, Fragment } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import MailIcon from '@material-ui/icons/Mail';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import { 
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Hidden,
+  Drawer,
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from '@material-ui/core';
+
+const drawerWidth = 240;
+
+const useStyles = (theme) => ({
+  drawer: {
+    [theme.breakpoints.up('sm')]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+});
+
+class NavBar extends Component {
+  state = {
+    title: 'Learning Django, React, and Material-UI',
+    drawerOpen: false,
+  };
+
+  handleDrawerToggle = () => {
+    this.setState({ drawerOpen: !this.state.drawerOpen });    
+  };
+
+  render() {
+    const { classes, window } = this.props;
+    const { title, drawerOpen } = this.state;
+    const container = window !== undefined ? () => window().document.body : undefined;
+    const drawer = (
+      <Fragment>
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Fragment>
+    );
+
+    return (
+      <Fragment>
+        <AppBar 
+          position="fixed" 
+          className={classes.appBar}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={this.handleDrawerToggle}
+              className={classes.menuButton}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography 
+              variant="h6" 
+              noWrap
+            >
+              {title}
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <nav 
+          className={classes.drawer} 
+          aria-label="drawer"
+        >
+          <Hidden smUp implementation="css">
+            <Drawer
+              container={container}
+              variant="temporary"
+              anchor="left"
+              open={drawerOpen}
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              onClose={this.handleDrawerToggle}
+              ModalProps={{
+                keepMounted: true, // Better open performance on mobile.
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+          <Hidden xsDown implementation="css">
+            <Drawer
+              variant="permanent"
+              open={true}
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+            >
+              {drawer}
+            </Drawer>
+          </Hidden>
+        </nav>
+      </Fragment>
+    );
+  }
+}
+
+export default withStyles(useStyles)(NavBar);
+
+/*
+import NotFoundPage from './NotFoundPage';
+import HomePage from './HomePage';
+import TodoPage from './todo/TodoPage';
+*/
+/*
+  <Switch>
+    <Route path="/" component={HomePage} exact />
+    <Route path="/todos" component={TodoPage} />
+    <Route component={NotFoundPage} />
+  </Switch>
+ */
+
+/*
 import { Container, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-class NavBar extends Component {
+
   render() {
     const links = [
       { 
@@ -43,6 +208,4 @@ class NavBar extends Component {
       </header>
     );
   }
-}
-
-export default NavBar;
+*/
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,32 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from './redux/store';
 import App from './App';
-import './index.css';
 
-
-
-/*
-
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
-import 'bootstrap/dist/js/bootstrap.js';
-*/
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider 
+      store={configureStore()}
+    >
+      <App />
+    </Provider>    
   </React.StrictMode>,
   document.getElementById('root')
 );
 
 /*
+import { BrowserRouter as Router } from 'react-router-dom';
+
     <Router>      
       <NavBar />
-      <Provider 
-        store={configureStore()}
-      >
+
         <App />
       </Provider>
     </Router>
