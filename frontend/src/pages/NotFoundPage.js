0b93cb1ba4fd35b84a138fa2da711e7b6a189344
Younghwan Mun <mcodegeeks@gmail.com>
2021-02-20 17:34:20 -0500

    Added error images

--- frontend/src/pages/NotFoundPage.js
+++ frontend/src/pages/NotFoundPage.js
@@ -1,28 +1,43 @@
 import { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
+import err404 from '../static/images/err404.gif';
 
 const useStyles = (theme) => ({
-  root: {
+  root: { 
     width: '100%',
   },
+  media: {
+    maxWidth: '100%', 
+  }
 });
 
 class NotFoundPage extends Component {
   componentDidMount() {
     if (this.props.setTitle)
       this.props.setTitle('Error 404');
+    if (this.props.setContentFlex)
+      this.props.setContentFlex(true);
   }
 
   render() {
     const { classes } = this.props;
 
     return (
-      <div className={classes.root}>
-        <Typography variant="h5">
-          Page Not Found
-        </Typography>
-      </div>
+      <Box 
+        className={classes.root}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        pl={3}
+        pb={3}
+      >
+        <img 
+          className={classes.media}
+          src={err404} 
+          alt="Error 404" 
+        />
+      </Box>
     );
   }
 }
