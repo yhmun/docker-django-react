05a00e5a5ef589ba233f3c57e6b88454ba3ec86c
Younghwan Mun <mcodegeeks@gmail.com>
2021-02-20 16:13:47 -0500

    Relocated router component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from './redux/store';
 import App from './App';
@@ -8,11 +7,9 @@ import "./index.css";
 
 ReactDOM.render(
   <React.Fragment>
-    <Router>
-      <Provider store={configureStore()}>
-        <App />
-      </Provider>
-    </Router>
+    <Provider store={configureStore()}>
+      <App />
+    </Provider>
   </React.Fragment>,
   document.getElementById('root')
 );
